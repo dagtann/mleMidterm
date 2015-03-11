579ee6af714700418c2c6ad9ab357ef0572184cf
dagtann <dag.tanneberg@googlemail.com>
2015-03-11 10:42:17 -0400

    Add minor revisions and comments to source, polish draft

--- R/setup.R
+++ R/setup.R
@@ -8,52 +8,8 @@ if(Sys.info()['user']=='dag') {
 library('MASS'); library('ggplot2'); library('ggthemes')
 set.seed(6886)
 
-## --- setup random data ----------------------------------------
-N <- 1000
-mu <- c(0, 0, 0)
-sigma <- matrix(
-  c(
-     1, .6, .4,
-    .6,  1,  0,
-    .4,  0,  1
-  ),
-  byrow = TRUE, ncol = 3
-)
-dta <- data.frame(mvrnorm(N, mu, sigma))
-colnames(dta) <- c('y', 'x', 'z')
-
-missMatrix <- cbind(
-  rbinom(N, 1, .9), rbinom(N, 1, .85), rbinom(N, 1, .83)
-)
-missMatrix[missMatrix==0] <- NA
-dta <- dta * missMatrix
-summary(dta)                  ## 40 Missing data points generated
-
 ## --- load ols function ----------------------------------------
-source(file.path(pathScript, 'olsFunc.R'))
-
-## --- test run -------------------------------------------------
-form <- formula(y ~ x + z)
-
-ols(form, data = dta)
-modelListDel <- ols(form, data = dta)
-set.seed(6886); modelImpDta <- ols(form, data = dta, impute = TRUE)
-
-referenceListDel <- lm(form, data = dta)
-set.seed(6886); referenceImpData <- lm(
-  form, data = Amelia::amelia(dta, m = 1)[['imputations']][[1]]
-)
-modelListDel; summary(referenceListDel)      ## results identical
-modelImpDta; summary(referenceImpData)  ## results subst. similar
-
-## Do components output correctly?
-modelListDel$coefficients                          ## output okay
-modelListDel$varcov                                ## output okay
-modelListDel$Rsq                                   ## output okay
-modelListDel$Fstat                                 ## output okay
-rm(mu, sigma, dta, form, missMatrix, modelListDel, modelImpDta,
-  referenceListDel, referenceImpData, N
-)
+source(file.path(pathScript, 'ols.R'))
 
 ## --- Load midterm data ----------------------------------------
 load(file.path(pathData, 'midTermData.rda'))
@@ -69,6 +25,15 @@ set.seed(6886); modelAmeliaReference <- lm(
   form, 
   data = Amelia::amelia(dataMiss, m = 1)[['imputations']][[1]]
 )
+set.seed(6886)
+coef(lm(
+  form, 
+  data = Amelia::amelia(dataMiss, m = 1)[['imputations']][[1]]
+))
+coef(lm(
+  form, 
+  data = Amelia::amelia(dataMiss, m = 1)[['imputations']][[1]]
+))
 
 ## --- Close up coefficients ------------------------------------
 modelAmelia[['coefficients']][, 1]; coef(modelAmeliaReference)
--- R/spaghettiPolity2.R
+++ R/spaghettiPolity2.R
@@ -5,9 +5,10 @@ scen.data <- cbind(
   polity2 = seq(4, 10, 1)
 )
 
+## --- Impute data & save coefficients ---------------------
 N <- 100
 set.seed(6886)
-seeds <- sample(1000:10000, N)
+seeds <- sample(1000:10000, N) ## reproducible seeds for Amelia
 seeds[1] <- 6886
 coefs <- matrix(NA, nrow = 3, ncol = N, 
   dimnames = list(
@@ -15,28 +16,28 @@ coefs <- matrix(NA, nrow = 3, ncol = N,
     paste0('seed', seeds)
   )
 )
-for(i in 1:N){
+for(i in 1:N){                ## Impute by seed and extract coefs
   set.seed(seeds[i])
   coefs[, i] <- ols(
     form, data = dataMiss, impute = TRUE
   )[['coefficients']][, 1]
 }
 
-## --- Predictions for each data structure ----------------------
+## --- Predictions for each missing data regime -----------------
 preds <- data.frame(scen.data, scen.data %*% coefs)
-preds <- reshape2::melt(
+preds <- reshape2::melt(              ## imputed data predictions
   data = preds,
   id.vars = 'polity2',
   measure.vars = paste0('seed', seeds)
 )
-preds.true <- data.frame(
+preds.true <- data.frame(          ## no missing data predictions
   scen.data, 
   yhat = predict(
     lm(formula = form, data = data), 
     newdata = data.frame(scen.data)
   )
 )
-preds.miss <- data.frame(
+preds.miss <- data.frame(             ## list wise deletion preds
   scen.data, 
   yhat = predict(
     lm(formula = form, data = dataMiss), 
@@ -45,8 +46,8 @@ preds.miss <- data.frame(
 )
 
 ## --- Proceed to plotting --------------------------------------
-p <- ggplot() +
-geom_line(
+p <- ggplot() +         ## empty plot, manual layering given data
+geom_line(        ## NOTE: Linetype scale is generated on the fly
   data = preds.true, 
   aes(x = polity2, y = yhat, linetype = 'No missings')
 ) +
@@ -66,8 +67,8 @@ geom_line(
 geom_line(
   data = preds, 
   aes(x = polity2, y = value,  group = variable), 
-  alpha = .12
-) +
+  alpha = .1
+) +                                  ## scale & label adjustments
 scale_linetype_manual(values = c(2, 4, 1, 3)) +
 scale_y_continuous(limits = c(-1, 1.2), breaks = seq(-1, 1.5, .5)) +
 scale_x_continuous(limits = c(4, 10), breaks = 4:10) +
@@ -75,7 +76,7 @@ labs(
   x = 'Polity 2',
   y = 'Net income inequality\n(Z-standardized)',
   linetype = 'Missing data'
-) +
+) +                                 ## finishing touches to theme
 theme_bw() +
 theme(
   text = element_text(family = 'serif'),
